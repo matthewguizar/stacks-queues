3df1d6c9f0a6f2411a0ad54ae443d6e4952bc20d
Matthew Guizar <matthewrguizar@gmail.com>
2023-03-08 20:08:16 -0800

    added print_stack method for testing

--- stackem.py
+++ stackem.py
@@ -10,3 +10,8 @@ class Stack:
         self.top = new_node
         self.height = 1
 
+    def print_stack(self):
+        temp = self.top
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
